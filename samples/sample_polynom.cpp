184b9476c65120f02f8109900f5a95ccd20088ac
MADZEROPIE <ask_ii1@mail.ru>
2020-02-04 20:51:46 +0300

    .

--- samples/sample_polynom.cpp
+++ samples/sample_polynom.cpp
@@ -6,15 +6,13 @@ int main(int argc, char** argv)
 {
 	setlocale(LC_ALL, "Russian");
 	Polynom a;
-	Polynom b("10x3y4z5+5z1");
+	Polynom b("10x3y4z5");
 	Polynom c("x3");
 	
 	a=b*c;
-	//b.clear();
-	//Polynom::Monom a,b;
-	
-	std::cout << a.ToString()<<'\n';//, "x2 - y2 + z2";
-	std::cout << c.ToString();// "y2";
+
+	std::cout << a.ToString()<<'\n';
+	std::cout << c.ToString();
 	
 	return 0;
 }
